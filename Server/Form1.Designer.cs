1334c8e4b301b9343bbbb33568ac3f9bb02220cc
mattias <mattias9922@hotmail.com>
2019-02-15 11:52:15 +0100

    Add project files.

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbxLogger = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbxLogger);
+            this.groupBox1.Location = new System.Drawing.Point(17, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 306);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chatlog";
+            // 
+            // lbxLogger
+            // 
+            this.lbxLogger.FormattingEnabled = true;
+            this.lbxLogger.Location = new System.Drawing.Point(8, 17);
+            this.lbxLogger.Name = "lbxLogger";
+            this.lbxLogger.Size = new System.Drawing.Size(272, 277);
+            this.lbxLogger.TabIndex = 0;
+            //this.lbxLogger.SelectedIndexChanged += new System.EventHandler(this.lbxLogger_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 341);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lbxLogger;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        TcpListener lyssnare;
+        TcpClient klient;
+        int port = 12345;
+        public List<TcpClient> klienter = new List<TcpClient>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lyssnare = new TcpListener(IPAddress.Any, port);
+                lyssnare.Start();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Server_form1: " + error.Message, Text);
+                return;
+            }
+            StartaMottagning();
+
+        }
+        public async void StartaMottagning()
+        {
+            try
+            {
+                klient = await lyssnare.AcceptTcpClientAsync();
+                lbxLogger.Items.Add("En klient har anslutit: " + klient.Client.RemoteEndPoint);
+                klienter.Add(klient);
+            
+                foreach (TcpClient i in klienter)
+                {
+                    lbxLogger.Items.Add("klienter anslutna: " + i.Client.RemoteEndPoint);
+                   
+                }
+
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Server_Starta mottagning: " + error.Message, Text);
+                return;
+            }
+            StartaLäsning(klient);
+            StartaMottagning();
+           
+        }
+        public async void StartaLäsning(TcpClient k)
+        {
+            byte[] buffert = new byte[1024];
+
+            int n = 0;
+            try
+            {
+                
+                n = await k.GetStream().ReadAsync(buffert, 0, buffert.Length);
+                lbxLogger.Items.Add("Starta läsning, n: " + n.ToString());
+                
+            }
+            catch (Exception error)
+            {
+                foreach (TcpClient i in klienter)
+                {
+                    lbxLogger.Items.Add("DC klienter : " + i.Client.RemoteEndPoint);
+
+                }
+                MessageBox.Show("Server_StartaLäsning: " + error.Message, Text);
+                return;
+            }
+            string result = Encoding.Unicode.GetString(buffert, 0, n);
+            lbxLogger.Items.Add(result);
+            StartaSändning(result);
+            StartaLäsning(k);
+        }
+            public async void StartaSändning(string message)
+        {
+            byte[] utData = Encoding.Unicode.GetBytes(message);
+
+            try
+            {
+                foreach(TcpClient klient in klienter)
+                {
+                    await klient.GetStream().WriteAsync(utData, 0, utData.Length);
+                    lbxLogger.Items.Add("Starta sändning");
+                }
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Server_StartaSändning: " + error.Message, Text);
+                return;
+            }
+            //StartaLäsning(klient);
+            
+        }
+    }
+
+}
+
